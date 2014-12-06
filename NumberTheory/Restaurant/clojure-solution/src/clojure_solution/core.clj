56687cf6715275be706d9c3cbb3d94c510052b0d
josiah14 <praenato14@gmail.com>
2014-12-06 01:46:33 -0600

    finish NumberTheory/Restaurant Clojure solution

--- NumberTheory/Restaurant/clojure-solution/src/clojure_solution/core.clj
+++ NumberTheory/Restaurant/clojure-solution/src/clojure_solution/core.clj
@@ -1,6 +1,28 @@
 (ns clojure-solution.core)
+(require '[clojure.string :as string])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-int [s]
+  (Integer. s))
+
+(defn parse-line [s]
+  (map parse-int (string/split s #" ")))
+
+(defn get-loaf-dimensions []
+  (let [numLoaves (parse-int (read-line))]
+    (map #(%1) (repeat numLoaves (comp parse-line read-line)))))
+
+(def squares (map #(* % %) (range 1 1001)))
+
+(defn min-number-of-slices [dims]
+  (let [l              (first dims)
+        b              (second dims)
+        area           (* l b)]
+    (letfn [(perfect-slice-dimension? [slice-area]
+              (= 0 (+ (mod area slice-area)
+                      (mod b (int (Math/sqrt slice-area)))
+                      (mod l (int (Math/sqrt slice-area))))))]
+      (let [largest-square (last (filter perfect-slice-dimension? (take (min l b) squares)))]
+        (int (/ area largest-square))))))
+
+(defn -main [& args] (dorun (map println (map min-number-of-slices (get-loaf-dimensions)))))
+
